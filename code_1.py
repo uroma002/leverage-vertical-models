f45e1ede5124616255f3663a613b95d479823a3f
Uroma002 <146116449+uroma002@users.noreply.github.com>
2024-01-08 14:18:33 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    const data = generateRandomData();
+    });
+    }
+
+    return data;
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+}
+function generateRandomData() {
+
+    data.forEach(item => {
+    const data = [];
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+}
